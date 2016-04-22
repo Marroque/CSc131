63d71ae036b392d40263eac0c0f8bc590abb15b1
Marroque <xexaxmx@gmail.com>
2016-04-21 23:37:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/emarroquin/getgps/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.emarroquin.getgps;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    Button btnGetLocation;
+    Button btnGetAddress;
+    TextView textShowLocation;
+
+    AppLocationService appLocationService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textShowLocation = (TextView) findViewById(R.id.textShowLocation);
+        appLocationService = new AppLocationService(
+                MainActivity.this);
+
+        btnGetLocation = (Button) findViewById(R.id.btnGetLocation);
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Location gpsLocation = appLocationService
+                        .getLocation(LocationManager.GPS_PROVIDER);
+                if (gpsLocation != null) {
+                    double latitude = gpsLocation.getLatitude();
+                    double longitude = gpsLocation.getLongitude();
+                    String result = "Latitude: " + gpsLocation.getLatitude() +
+                            " Longitude: " + gpsLocation.getLongitude();
+                    textShowLocation.setText(result);
+                } else {
+                    showSettingsAlert();
+                }
+            }
+        });
+
+        btnGetAddress = (Button) findViewById(R.id.btnGetAddress);
+        btnGetAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Location location = appLocationService
+                        .getLocation(LocationManager.GPS_PROVIDER);
+
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    LocationAddress locationAddress = new LocationAddress();
+                    locationAddress.getAddressFromLocation(latitude, longitude,
+                            getApplicationContext(), new GeocoderHandler());
+                } else {
+                    showSettingsAlert();
+                }
+
+            }
+        });
+
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                MainActivity.this);
+        alertDialog.setTitle("SETTINGS");
+        alertDialog.setMessage("GPS is disabled! Please enable it in your settings to use this feature.");
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private class GeocoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            textShowLocation.setText(locationAddress);
+        }
+    }
+}
